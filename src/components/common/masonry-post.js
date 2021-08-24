dcdec0e1c6b8c02067e6f9ab79d021180e4bafc6
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2021-08-24 01:08:40 -0500

    Style masonry layout

--- src/components/common/masonry-post.js
+++ src/components/common/masonry-post.js
@@ -1,17 +1,28 @@
 import React from 'react';
+import { categoryColors } from './styles';
 
 function MasonryPost({ post, tagsOnTop }) {
 
-    const style = {backgroundImage: `url('${require(`../../assets/images/${post.image}`)}')`};
-    
+    const windowWidth = window.innerWidth;
+    // const imageBackground = {backgroundImage: `url("${require(`../../assets/images/${post.image}`)}")`,};
+    const imageBackground = {backgroundColor: `#000`};
+    const style = windowWidth > 900 ? { ...imageBackground, ...post.style } : imageBackground;
+
     return (
         <a className="masonry-post overlay" style={style} href={post.link}>
-            <div className="image-text">
+            <div className="image-text" style={{justifyContent: tagsOnTop ? 'space-between' : 'flex-end'}}>
+                <div className="tags-container">
+                    { post.categories.map((tag, index) =>
+                        <span className="tag" key={index} style={{backgroundColor: categoryColors[tag]}}>
+                            {tag.toUpperCase()}
+                        </span>
+                    )}
+                </div>
                 <div>
                     <h2 className="image-title">{post.title}</h2>
+                    <span className="image-date">{post.date}</span>
                 </div>
             </div>
-            {console.log(post.image)}
         </a>
     )
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,58 @@
 import React from 'react';
-import { PostMasonry } from '../components/common';
+import { PostMasonry, MasonryPost } from '../components/common';
 import trending from '../assets/mocks/trending';
+import featured from '../assets/mocks/featured';
+
+const trendingConfig = {
+    1: {
+        gridArea: '1 / 2 / 3 / 3',
+    }
+}
+
+const featuredConfig = {
+    0: {
+        gridArea: '1 / 1 / 2 / 3',
+        height: '300px'
+    },
+    1: {
+        height: '300px'
+    },
+    3: {
+        height: '630px',
+        marginLeft: '30px',
+        width: '600px',
+    }
+}
+
+const mergeStyles = function (posts, config) {
+    posts.forEach((post, index) => {
+        post.style = config[index]
+
+    })
+}
+
+mergeStyles(trending, trendingConfig)
+mergeStyles(featured, featuredConfig)
+
+const lastPost = featured.pop()
 
 function Home() {
     return (
-        <section className="home container"> 
-            <div className="row">
-                <h1>Trending Posts</h1>
-                <PostMasonry posts={ trending } columns={ 3 }/>
-            </div>
-        </section>
+        <main className="home">
+            <section className="container">
+                <div className="row">
+                    <section className="featured-posts-container">
+                        <PostMasonry posts={featured} columns={2} tagsOnTop={true} />
+                        <MasonryPost post={lastPost} tagsOnTop={true} />
+                    </section>
+                </div>
+            </section>
+            <section className="container">
+                <div className="row">
+                    <PostMasonry posts={trending} columns={3}/>
+                </div>
+            </section>
+        </main>
     )
 }
 
